2600c55dc798747ab121eebb7942d040f9652ff1
rajatrsp2001 <69463332+rajatrsp2001@users.noreply.github.com>
2020-08-10 18:36:26 +0530

    Add files via upload

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,25 @@
+makeCachematrix <- function(x=matrix()){
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function(){x}
+  setinverse <- function(inverse) {inv <<-inverse}
+  getinverse <- function() {inv}
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+cachesolve <- function(x,...){
+  inv <- x$getInverse()
+    if(!is.null(inv)){
+      message("getting cached data")
+      return(inv)
+      
+    }
+  mat <- x$get()
+  inv <- solve(mat,...)
+  x$setinverse(inv)
+  
+  
+}
